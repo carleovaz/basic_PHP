370899e57a353a87e1cad8eb1487b39af3eba153
carleovaz <73903458+carleovaz@users.noreply.github.com>
2021-11-06 19:45:15 +0100

    Update control1.php

--- control1.php
+++ control1.php
@@ -9,8 +9,12 @@
 	<?php
     /*Crea un array con notas aleatorias de una asignatura. Si la media de las notas da aprobado, muestra por pantalla un mensaje de felicitación. 
     En caso contrario muestra un mensaje con la media que ha sacado.*/
-    $asignatura =[2,4,6,8,10];
-    $media = "Tu nota media de la asignatura es: " . array_sum($asignatura)/count($asignatura) . "<br/>";
+    $notas[0] = rand(1,10);
+    $notas[1] = rand(1,10);
+    $notas[2] = rand(1,10);
+    $notas[3] = rand(1,10);
+    $notas[4] = rand(1,10);
+    $media = "Tu nota media de la asignatura es: " . array_sum($notas)/count($notas) . "<br/>";
     echo $media;
     if($media >= 5)
     {
